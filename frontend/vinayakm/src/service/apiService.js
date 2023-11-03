4b8a204f534913d7cac9072c6210a5e72c59339f
Yash Bharadwaj <yashmks1998@gmail.com>
2023-11-03 17:45:50 +0530

    ip changes

--- frontend/vinayakm/src/service/apiService.js
+++ frontend/vinayakm/src/service/apiService.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000', // Replace with your API base URL
+  baseURL: 'http://vinayakm.co.in:3000', // Replace with your API base URL
 });
 
 // Set the default headers here
