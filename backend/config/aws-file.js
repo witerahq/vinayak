621efea532c1bae03172643b0d0d665cc55053f6
VipulChikara <vipul.chikara.6002@gmail.com>
2023-11-27 11:25:58 +0530

    Upload file migration

--- backend/config/aws-file.js
+++ backend/config/aws-file.js
@@ -1,36 +1,37 @@
-const aws = require('aws-sdk');
+// Update AWS SDK to version 3
+const { S3Client } = require('@aws-sdk/client-s3');
+const { fromIni } = require('@aws-sdk/credential-provider-ini');
+
+// Update Multer to version 2
 const multer = require('multer');
-const multerS3 = require('multer-s3');
+const { S3 } = require('multer-s3');
 
-aws.config.update({
-    accessKeyId: 'AKIAWHHWWWPBOCGSTYF6',
-    secretAccessKey: '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',
-    region: 'ap-south-1', // 
+const s3 = new S3Client({
+    credentials: fromIni(),
+    region: 'ap-south-1',
 });
-
-const s3 = new aws.S3();
 
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
+        cb(null, true);
     } else {
-      cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+        cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
     }
-  }
+};
 
 const upload = multer({
     fileFilter,
-    storage: multerS3({
-      s3,
-      bucket: 'demo.moveies.com',
-      acl: 'public-read',
-      metadata: function (req, file, cb) {
-        cb(null, {fieldName: file.fieldname});
-      },
-      key: function (req, file, cb) {
-        cb(null, Date.now().toString())
-      }
-    })
-  })
+    storage: new S3({
+        client: s3,
+        bucket: 'demo.moveies.com',
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString());
+        },
+    }),
+});
 
-  module.exports = upload;
+module.exports = upload;
--- backend/controllers/fileUploadController.js
+++ backend/controllers/fileUploadController.js
@@ -1,4 +1,5 @@
-const AWS = require('aws-sdk');
+const { S3Client } = require('@aws-sdk/client-s3');
+const { fromIni } = require('@aws-sdk/credential-provider-ini');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
@@ -10,13 +11,10 @@ const AWS_SECRET_ACCESS_KEY = 'rV0SblN+/tLOXREDqVvL+h4EZp1GpO4MkOH0AGVx';
 const AWS_BUCKET_NAME = 'vinayakmudit';
 
 // Initialize the AWS SDK
-AWS.config.update({
-  accessKeyId: AWS_ACCESS_KEY_ID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-   region: 'ap-south-1'
+const s3 = new S3Client({
+  credentials: fromIni(),
+  region: 'ap-south-1',
 });
-
-const s3 = new AWS.S3();
 
 
 function uploadFile(req, res) {
