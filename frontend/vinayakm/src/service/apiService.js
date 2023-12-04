bee3268ba32127a51999ba90c56d5e6b8b38f337
VipulChikara <vipul.chikara.6002@gmail.com>
2023-12-05 04:48:12 +0530

    updates as per the doctor requirement

--- frontend/vinayakm/src/service/apiService.js
+++ frontend/vinayakm/src/service/apiService.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://vinayakm.co.in:3000',
-  baseURL: 'https://localhost:3000',
+  baseURL: 'https://vinayakm.co.in:3000',
+  // baseURL: 'https://localhost:3000',
 
    // Replace with your API base URL
 });
